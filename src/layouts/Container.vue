2b564465a62116af8908deea34bc81d0fd128f12
ZhenyaGro <bon4ios@gmail.com>
2024-08-12 20:11:29 +0300

    Создан компонент контейнер

---
+++ src/layouts/Container.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="container">
+    <slot />
+  </section>
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from 'vue';
+
+defineComponent({
+  name: 'Container',
+});
+</script>
+
+<style scoped lang="scss">
+$containerPadding: calc(50% - 1200px / 2);
+
+.container {
+  padding-left: $containerPadding;
+  padding-right: $containerPadding;
+}
+</style>
