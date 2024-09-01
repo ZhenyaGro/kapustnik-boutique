2e3103a08f26f09b788b1ea076b11aa59686fc4d
ZhenyaGro <zhenyagro@icloud.com>
2024-09-01 21:43:01 +0300

    Добавлена стартовая страница

---
+++ src/pages/StartPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <Container
+    ><section class="start-page">
+      <img src="/img/logo/logoWithText.svg" class="start-page__logo" alt="Логотип" /></section
+  ></Container>
+</template>
+
+<script setup>
+import { defineComponent } from 'vue';
+import Container from '@/layouts/Container.vue';
+
+defineComponent({
+  name: 'StartPage',
+  components: {
+    Container,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@use '@/styles/colors';
+
+.start-page {
+  min-height: 100dvh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &__logo {
+    max-width: 600px;
+    opacity: 0;
+    animation: 3s show ease;
+    animation-delay: 0.5s;
+    animation-fill-mode: forwards;
+  }
+}
+
+@keyframes show {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+</style>
