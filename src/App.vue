bbd73ec2c5431ea352b4083ffd1f77a2eb63a22a
ZhenyaGro <zhenyagro@icloud.com>
2024-09-01 17:13:46 +0300

    Создан Header

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div class="app-container">
+    <Header />
+    <router-view />
+  </div>
 </template>
 
+<script lang="ts" setup>
+import { defineComponent } from 'vue';
+import Header from './components/Header.vue';
+
+defineComponent({
+  name: 'ApproveDocument',
+  components: {
+    Header,
+  },
+});
+</script>
+
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Roboto', sans-serif, Helvetica;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,75 @@
+<template>
+  <Container class="header">
+    <div class="header__content">
+      <a href="#"><img src="/img/logoWithBigTextWhite.svg" class="logo" alt="Логотип" /></a>
+      <nav class="navbar">
+        <a href="#" class="navbar__link">Главная</a>
+        <a href="#" class="navbar__link">Ассортимент</a>
+        <a href="#" class="navbar__link">Отзывы</a>
+        <a href="#" class="navbar__link">Как заказать</a>
+        <a href="#" class="navbar__link">Контакты</a>
+      </nav>
+      <a href="tel:+79212733414">+7 (921) 273-34-14</a>
+    </div>
+  </Container>
+</template>
+
+<script setup>
+import { defineComponent } from 'vue';
+import Container from '@/layouts/Container.vue';
+
+defineComponent({
+  name: 'Header',
+  components: {
+    Container,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@use '@/styles/colors';
+
+.header {
+  background: -webkit-linear-gradient(
+    90deg,
+    colors.$additionalColor,
+    colors.$brandColor,
+    colors.$additionalColor
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    90deg,
+    colors.$additionalColor,
+    colors.$brandColor,
+    colors.$additionalColor
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+  .header__content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    a {
+      color: white;
+      text-decoration: none;
+    }
+
+    .logo {
+      height: 80px;
+    }
+
+    .navbar {
+      display: flex;
+
+      &__link {
+        display: block;
+        padding: 38px 12px;
+        transition: background-color 0.2s;
+
+        &:hover {
+          background-color: colors.$additionalColor;
+        }
+      }
+    }
+  }
+}
+</style>
