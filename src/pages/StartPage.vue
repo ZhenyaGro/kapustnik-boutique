0580a4044f345d3c610a4c9088a745c01542f7d2
ZhenyaGro <zhenyagro@icloud.com>
2024-09-05 23:06:24 +0300

    Стартовая страница

--- src/pages/StartPage.vue
+++ src/pages/StartPage.vue
@@ -1,13 +1,23 @@
 <template>
   <Container
     ><section class="start-page">
-      <img src="/img/logo/logoWithText.svg" class="start-page__logo" alt="Логотип" /></section
-  ></Container>
+      <img src="/img/logo/logoWithText.svg" class="start-page__logo" alt="Логотип" />
+      <h1>Магазин модной женской одежды больших размеров со стразами</h1>
+    </section>
+  </Container>
+  <Container :id="pageLinks.start.id">
+    <a :href="pageLinks.products.url" class="start-content">
+      <img src="/img/photos/model1.jpeg" alt="Фото модель" />
+      <img src="/img/photos/model2.jpeg" alt="Фото модель" />
+      <img src="/img/photos/shop.jpg" alt="Интерьер магазина" />
+    </a>
+  </Container>
 </template>
 
 <script setup>
 import { defineComponent } from 'vue';
 import Container from '@/layouts/Container.vue';
+import { pageLinks } from '@/constants/pageLinks';
 
 defineComponent({
   name: 'StartPage',
@@ -23,8 +33,10 @@ defineComponent({
 .start-page {
   min-height: 100dvh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  gap: 60px;
 
   &__logo {
     max-width: 600px;
@@ -32,6 +44,42 @@ defineComponent({
     animation: 3s show ease;
     animation-delay: 0.5s;
     animation-fill-mode: forwards;
+  }
+
+  h1 {
+    max-width: 1030px;
+    opacity: 0;
+    font-weight: 400;
+    color: black;
+    animation: 3s show ease;
+    animation-delay: 1.5s;
+    animation-fill-mode: forwards;
+  }
+}
+
+.start-content {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: auto auto;
+  gap: 20px;
+  width: 100%;
+
+  img {
+    width: 100%;
+    height: auto;
+    border-radius: 36px;
+  }
+
+  img:nth-child(3) {
+    grid-column: 1 / 3;
+  }
+}
+
+img {
+  transition: transform 0.5s;
+
+  &:hover {
+    transform: scale(1.05);
   }
 }
 
