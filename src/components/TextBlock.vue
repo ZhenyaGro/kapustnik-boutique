fc4e91c9f541ffa22d599acb0acbc368be33e7cc
ZhenyaGro <zhenyagro@icloud.com>
2024-09-17 22:27:40 +0300

    Создан компонент текстового блока

---
+++ src/components/TextBlock.vue
@@ -0,0 +1,31 @@
+<template>
+  <article class="text-block">
+    <slot />
+  </article>
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from 'vue';
+
+defineComponent({
+  name: 'TextBlock',
+});
+</script>
+
+<style lang="scss" scoped>
+@use '@/styles/colors';
+
+.text-block {
+  max-width: 800px;
+  padding: 40px;
+  margin: 0 auto 30px;
+  border-radius: 16px;
+  color: white;
+  font-size: 20px;
+  background-color: colors.$brandColor;
+}
+
+::v-deep p:not(:last-child) {
+  margin-bottom: 16px;
+}
+</style>
