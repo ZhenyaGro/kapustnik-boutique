4a560bd8cc782c816cb442d0c416bb400d284b58
ZhenyaGro <zhenyagro@icloud.com>
2024-10-02 18:03:41 +0300

    Создан компонент контакты

---
+++ src/pages/Contacts.vue
@@ -0,0 +1,103 @@
+<template>
+  <Container :id="pageLinks.contacts.id" class="contacts anchor">
+    <TextBlock>
+      <h1 class="heading">Контакты</h1>
+      <address class="contact-info">
+        <p>
+          <a href="https://yandex.ru/maps/-/CDT7iF3Z" target="_blank"
+            ><strong>Адрес: </strong>город Санкт-Петербург, Гражданский проспект, 41<br />
+            ТЦ Академический, 1 этаж, Павильон А56</a
+          ><br />
+          Метро <span class="subway">Академическая</span><br />
+          <strong>Время работы: </strong>С 10:00 до 22:00 каждый день
+        </p>
+        <p><strong>Телефон: </strong><a href="tel:+79212733414">+7&nbsp;(921)&nbsp;273-34-14</a></p>
+        <p><strong>Email: </strong><a href="mailto:senichev260483@mail.ru">senichev260483@mail.ru</a></p>
+      </address>
+      <section>
+        <h3>Мы в социальных сетях:</h3>
+        <nav class="social-links">
+          <a
+            v-for="(socialMediaSource, index) in SOCIAL_MEDIA_LINKS"
+            :key="index"
+            :href="socialMediaSource.url"
+            target="_blank"
+            :title="socialMediaSource.text"
+          >
+            <img class="social-links__logo" :src="socialMediaSource.imgUrl" :alt="socialMediaSource.text" />
+          </a>
+        </nav>
+      </section>
+      <section>
+        <h3>Мы на карте:</h3>
+        <MapYandex />
+      </section>
+    </TextBlock>
+  </Container>
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from 'vue';
+import Container from '@/layouts/Container.vue';
+import { pageLinks } from '@/constants/pageLinks';
+import TextBlock from '@/components/TextBlock.vue';
+import { SOCIAL_MEDIA_LINKS } from '@/constants/socialMediaLinks';
+import MapYandex from '@/components/MapYandex.vue';
+
+defineComponent({
+  name: 'Contacts',
+  components: {
+    Container,
+    MapYandex,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@use '@/styles/colors';
+
+.contact-info {
+  .subway {
+    color: red;
+  }
+
+  a:active {
+    background-color: colors.$additionalColor;
+  }
+
+  @media (hover: hover) {
+    a:hover {
+      background-color: colors.$additionalColor;
+    }
+  }
+}
+
+h3 {
+  text-align: center;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+.social-links {
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+
+  &__logo {
+    width: 52px;
+    height: 52px;
+    transition: transform 0.3s;
+    user-select: none;
+
+    &:active {
+      transform: scale(1.1);
+    }
+
+    @media (hover: hover) {
+      &:hover {
+        transform: scale(1.1);
+      }
+    }
+  }
+}
+</style>
