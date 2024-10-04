fff446c9c62d0895cd0c387374a37e68f572f057
ZhenyaGro <zhenyagro@icloud.com>
2024-10-04 17:57:39 +0300

    Компоненты на странице

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <StartPage />
-    <StartContent />
-    <Products />
-    <Contacts />
+    <StartPage class="home__page" />
+    <StartContent class="home__page" />
+    <Products class="home__page" />
+    <Brands class="home__page" />
+    <HowToOrder class="home__page" />
+    <Contacts class="home__page" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import StartPage from '@/pages/StartPage.vue';
-import Products from '@/pages/Products.vue';
-import Contacts from '@/pages/Contacts.vue';
 import StartContent from '@/pages/StartContent.vue';
+import Products from '@/pages/Products.vue';
+import Brands from '@/pages/Brands.vue';
+import Contacts from '@/pages/Contacts.vue';
+import HowToOrder from '@/pages/HowToOrder.vue';
 
 export default defineComponent({
   name: 'HomeView',
@@ -20,7 +24,9 @@ export default defineComponent({
     StartPage,
     StartContent,
     Products,
+    Brands,
     Contacts,
+    HowToOrder,
   },
 });
 </script>
@@ -30,5 +36,9 @@ export default defineComponent({
 
 .home {
   padding-top: variables.$headerHeight;
+
+  &__page {
+    margin-bottom: 60px;
+  }
 }
 </style>
